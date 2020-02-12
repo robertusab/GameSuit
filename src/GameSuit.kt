5b387b423657fd88cb23cf1c887352de2a659586
robertusab <obetckx@gmail.com>
2020-02-12 23:52:07 +0700

    v 1.1 add error input

--- src/GameSuit.kt
+++ src/GameSuit.kt
@@ -3,25 +3,47 @@ import java.util.*
 class New() {
     companion object {
         @JvmStatic
+
         // user input
         fun main(args: Array<String>) {
-            println("Masukkan Pemain 1")
-            val pemainPertama = readLine()
-            println("Masukkan Pemain 2")
-            val pemainKedua = readLine()
+            var array = arrayOf("batu", "gunting", "kertas")
 
+            // Pemain 1
+            println("Masukkan Pemain 1")
+            var pemainPertama = readLine()
+            if (!array.contains(pemainPertama)) {
+                do {
+                    println("Formatnya salah cuy. Pilih batu, gunting atau kertas")
+                    pemainPertama = readLine()
+                } while (!array.contains(pemainPertama))
+
+            }
+
+            // Pemain 2
+            println("Masukkan Pemain 2")
+            var pemainKedua = readLine()
+            if (!array.contains(pemainKedua)) {
+                do {
+                    println("Yah, salah format. Pilih batu, gunting atau kertas")
+                    pemainKedua = readLine()
+                } while (!array.contains(pemainKedua))
+
+            }
             // if
             if ((pemainPertama.equals("batu") && pemainKedua.equals("gunting")) ||
-                    (pemainPertama.equals("gunting") && pemainKedua.equals("kertas")) ||
-                    (pemainPertama.equals("kertas") && pemainKedua.equals("batu")))
-            println("Pemain Pertama MENANG!")
+                (pemainPertama.equals("gunting") && pemainKedua.equals("kertas")) ||
+                (pemainPertama.equals("kertas") && pemainKedua.equals("batu"))
+            )
+                println("Pemain Pertama MENANG!")
             if ((pemainPertama.equals("batu") && pemainKedua.equals("kertas")) ||
-                    (pemainPertama.equals("gunting") && pemainKedua.equals("batu")) ||
-                    (pemainPertama.equals("kertas") && pemainKedua.equals("gunting")))
-            println("Pemain Kedua MENANG!")
+                (pemainPertama.equals("gunting") && pemainKedua.equals("batu")) ||
+                (pemainPertama.equals("kertas") && pemainKedua.equals("gunting"))
+            )
+                println("Pemain Kedua MENANG!")
             if ((pemainPertama.equals("batu") && pemainKedua.equals("batu")) ||
                     (pemainPertama.equals("gunting") && pemainKedua.equals("gunting")) ||
-                    (pemainPertama.equals("kertas") && pemainKedua.equals("kertas")))
+                    (pemainPertama.equals("kertas") && pemainKedua.equals("kertas"))
+                    )
             println("DRAW!")
         }
     }
