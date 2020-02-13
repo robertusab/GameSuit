267e586c5a5e0099f8e2abeaeb4e723afc5d5545
robertusab <obetckx@gmail.com>
2020-02-13 17:02:44 +0700

    v 1.2 OOP

--- src/GameSuit.kt
+++ src/GameSuit.kt
@@ -29,22 +29,9 @@ class New() {
                 } while (!array.contains(pemainKedua))
 
             }
-            // if
-            if ((pemainPertama.equals("batu") && pemainKedua.equals("gunting")) ||
-                (pemainPertama.equals("gunting") && pemainKedua.equals("kertas")) ||
-                (pemainPertama.equals("kertas") && pemainKedua.equals("batu"))
-            )
-                println("Pemain Pertama MENANG!")
-            if ((pemainPertama.equals("batu") && pemainKedua.equals("kertas")) ||
-                (pemainPertama.equals("gunting") && pemainKedua.equals("batu")) ||
-                (pemainPertama.equals("kertas") && pemainKedua.equals("gunting"))
-            )
-                println("Pemain Kedua MENANG!")
-            if ((pemainPertama.equals("batu") && pemainKedua.equals("batu")) ||
-                    (pemainPertama.equals("gunting") && pemainKedua.equals("gunting")) ||
-                    (pemainPertama.equals("kertas") && pemainKedua.equals("kertas"))
-                    )
-            println("DRAW!")
+
+            val mekanik1 = mekanik()
+            mekanik1.cetakMekanik(pemainPertama, pemainKedua)
         }
     }
 }
