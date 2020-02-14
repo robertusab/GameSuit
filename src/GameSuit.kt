654891cbbfdbd4ffc163e79942815d665eacd4d4
robertusab <obetckx@gmail.com>
2020-02-14 12:12:35 +0700

    v 1.3 3 kali jalan

--- src/GameSuit.kt
+++ src/GameSuit.kt
@@ -8,30 +8,41 @@ class New() {
         fun main(args: Array<String>) {
             var array = arrayOf("batu", "gunting", "kertas")
 
+            // intro
+
+            println("===== MAIN SUIT =====")
+            println("Batu, Gunting, Kertas")
+            println("=====================")
             // Pemain 1
-            println("Masukkan Pemain 1")
-            var pemainPertama = readLine()
-            if (!array.contains(pemainPertama)) {
-                do {
-                    println("Formatnya salah cuy. Pilih batu, gunting atau kertas")
-                    pemainPertama = readLine()
-                } while (!array.contains(pemainPertama))
+            // bikin loop
+            var i = 0
+            do {
 
-            }
 
-            // Pemain 2
-            println("Masukkan Pemain 2")
-            var pemainKedua = readLine()
-            if (!array.contains(pemainKedua)) {
-                do {
-                    println("Yah, salah format. Pilih batu, gunting atau kertas")
-                    pemainKedua = readLine()
-                } while (!array.contains(pemainKedua))
+                println("Pemain 1 pilih apa?")
+                var pemainPertama = readLine()!!.toLowerCase()
+                if (!array.contains(pemainPertama)) {
+                    do {
+                        println("Formatnya salah cuy. Pilih Batu, Gunting atau Kertas")
+                        pemainPertama = readLine()!!.toLowerCase()
+                    } while (!array.contains(pemainPertama))
+                }
 
-            }
+                // Pemain 2
+                println("Yuk, Pemain 2?")
+                var pemainKedua = readLine()!!.toLowerCase()
+                if (!array.contains(pemainKedua)) {
+                    do {
+                        println("Yah, salah format. Pilih Batu, Gunting atau Kertas")
+                        pemainKedua = readLine()!!.toLowerCase()
+                    } while (!array.contains(pemainKedua))
+                }
 
-            val mekanik1 = mekanik()
-            mekanik1.cetakMekanik(pemainPertama, pemainKedua)
+                val mekanik1 = mekanik()
+                mekanik1.cetakMekanik(pemainPertama, pemainKedua)
+                println(if (i <2 )"Lanjut" else "Selasai")
+                i++
+            } while (i <3)
         }
     }
 }
--- src/mekanik.kt
+++ src/mekanik.kt
@@ -1,19 +1,16 @@
 class mekanik() {
-    fun cetakMekanik(pemainPertama:String?, pemainKedua:String?) {
+    fun cetakMekanik(pemainPertama: String?, pemainKedua: String?) {
         if ((pemainPertama.equals("batu") && pemainKedua.equals("gunting")) ||
             (pemainPertama.equals("gunting") && pemainKedua.equals("kertas")) ||
             (pemainPertama.equals("kertas") && pemainKedua.equals("batu"))
         )
             println("Pemain Pertama MENANG!")
-        if ((pemainPertama.equals("batu") && pemainKedua.equals("kertas")) ||
+        else if ((pemainPertama.equals("batu") && pemainKedua.equals("kertas")) ||
             (pemainPertama.equals("gunting") && pemainKedua.equals("batu")) ||
             (pemainPertama.equals("kertas") && pemainKedua.equals("gunting"))
         )
             println("Pemain Kedua MENANG!")
-        if ((pemainPertama.equals("batu") && pemainKedua.equals("batu")) ||
-            (pemainPertama.equals("gunting") && pemainKedua.equals("gunting")) ||
-            (pemainPertama.equals("kertas") && pemainKedua.equals("kertas"))
-        )
+        else if (pemainPertama.equals(pemainKedua))
             println("DRAW!")
     }
 }
